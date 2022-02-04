21baa97a273fdca8629ffd87e41e314e7bbb3343
maaxxaam <maaxxaam@protonmail.com>
2022-02-04 15:19:55 +0300

    build v1

--- scripts/funcs.js
+++
@@ -1,8 +0,0 @@
-function getSymbol(str, index) { return str[index]; }
-
-function getState(guess, word, index){
-	var real_index = word.indexOf(guess[index]);
-	if (real_index === -1) return 0;
-	if (real_index == index) return 2;
-	return 1;
-}
