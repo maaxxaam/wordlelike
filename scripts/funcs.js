ad7ce7be24e0d6bbf2351655ef0f13dab99da7de
maaxxaam <maaxxaam@protonmail.com>
2022-02-06 15:50:52 +0300

    v2

--- scripts/funcs.js
+++ scripts/funcs.js
@@ -1,8 +1,24 @@
-function getSymbol(str, index) { return str[index]; }
+function count(str, char) {
+	var count = 0,
+		pos = str.indexOf(char);
+
+	while (pos > -1) {
+		++count;
+		pos = str.indexOf(char, ++pos);
+	}
+	return count;
+}
 
 function getState(guess, word, index){
-	var real_index = word.indexOf(guess[index]);
+	var letter = guess[index];
+	var real_index = word.indexOf(letter);
 	if (real_index === -1) return 0;
+	else {
+		var sub = guess.substr(0, index);
+		var cnt = count(sub, letter);
+		var or_cnt = count(word, letter);
+		if (or_cnt <= cnt) return 0;
+ 	};
 	if (real_index == index) return 2;
 	return 1;
 }
